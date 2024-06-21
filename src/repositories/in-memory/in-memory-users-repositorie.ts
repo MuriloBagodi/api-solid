073d54e3693f64e10977880862a9205f217af835
Murilo Bagodi <murilosbagodi@hotmail.com>
2024-06-21 11:59:16 -0300

    Chore: In-memory DB to Make unit test a lot faster and to not make our db dirty

---
+++ src/repositories/in-memory/in-memory-users-repositorie.ts
@@ -0,0 +1,36 @@
+import { Prisma, User } from '@prisma/client'
+import { UsersRepository } from '../usersRepository'
+
+export class InMemoryUsersRepository implements UsersRepository {
+  public items: User[] = []
+  async create(data: Prisma.UserCreateInput) {
+    const user = {
+      id: 'any_id',
+      name: data.name,
+      email: data.email,
+      password: data.password,
+      created_at: new Date(),
+    }
+    this.items.push(user)
+
+    return user
+  }
+
+  async findByEmail(email: string) {
+    const user = this.items.find((item) => item.email === email)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+}
+
+// async findByEmail() {
+//   return null
+// },
+
+// async create(data) {
+//
+// },
--- src/services/register.spec.ts
+++ src/services/register.spec.ts
@@ -1,24 +1,25 @@
 import { RegisterUseCase } from '@/services/register'
 import { expect, it, describe } from 'vitest'
 import { compare } from 'bcrypt'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repositorie'
+import { UserAlreadyExistsError } from './errors_handlers/user-already-exists-error'
 
 describe('Register Use Case', () => {
-  it('Should be hashed the user password', async () => {
-    const registerUseCase = new RegisterUseCase({
-      async findByEmail() {
-        return null
-      },
+  it('Should not be able to register', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
 
-      async create(data) {
-        return {
-          id: 'any_id',
-          name: data.name,
-          email: data.email,
-          password: data.password,
-          created_at: new Date(),
-        }
-      },
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'jhondoe@example.com',
+      password: 'password123',
     })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+  it('Should be hashed the user password', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
 
     const { user } = await registerUseCase.execute({
       name: 'John Doe',
@@ -32,5 +33,25 @@ describe('Register Use Case', () => {
     )
 
     expect(isPasswordCorrectlyHashed).toEqual(true)
+  })
+  it('Should not be able to register with same email twice', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    const email = 'johndoe@example.com'
+
+    await registerUseCase.execute({
+      name: 'John Doe',
+      email,
+      password: 'password123',
+    })
+
+    expect(() =>
+      registerUseCase.execute({
+        name: 'John Doe',
+        email,
+        password: 'password123',
+      }),
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError)
   })
 })
